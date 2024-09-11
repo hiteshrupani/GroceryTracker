ae8140d6c9c77bbc80725691d969ea17a2de7c75
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-09-11 15:30:10 +0530

    Initial Commit

---
+++ GroceryTracker/GroceryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTrackerApp.swift
+//  GroceryTracker
+//
+//  Created by Hitesh Rupani on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
