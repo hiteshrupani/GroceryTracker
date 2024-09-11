1c0fcee5e6f548eb2b254f92f45e6220e3db8ae9
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-09-11 20:23:25 +0530

    added login view with button and text field view

---
+++ GroceryTracker/Views/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  GroceryTracker
+//
+//  Created by Hitesh Rupani on 11/09/24.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
---
+++ GroceryTracker/Views/TextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldView.swift
+//  GroceryTracker
+//
+//  Created by Hitesh Rupani on 11/09/24.
+//
+
+import SwiftUI
+
+struct TextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldView()
+}
